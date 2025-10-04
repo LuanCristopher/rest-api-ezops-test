4a73b837aa6d4a6d969f972d754c150aff76191c
LuanCristopher <luangugi@gmail.com>
2025-10-04 19:12:20 -0300

    feat: fix: db connection

--- server/infra/database.js
+++ server/infra/database.js
@@ -1,10 +1,13 @@
 const pgp = require('pg-promise')();
 const db = pgp({
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	database: process.env.DB_NAME
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = db;
