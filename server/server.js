6056401f7a4bb6ef2e451f246ebcafa790d5a771
LuanCristopher <luangugi@gmail.com>
2025-10-05 05:48:19 -0300

    fix: json.package and server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,31 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors({
+  origin: 'https://app.luanoliveira.exam.ezopscloud.tech',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+
 app.use(express.json());
+
 app.use('/', require('./route/postsRoute'));
+
 app.use(function (error, req, res, next) {
-	if (error.message === 'Post already exists') {
-		return res.status(409).send(e.message);
-	}
-	if (error.message === 'Post not found') {
-		return res.status(404).send(e.message);
-	}
-	res.status(500).send(error.message);
+  console.error('❌ Error handler:', error.message);
+
+  if (error.message === 'Post already exists') {
+    return res.status(409).send(error.message);
+  }
+  if (error.message === 'Post not found') {
+    return res.status(404).send(error.message);
+  }
+  res.status(500).send(error.message);
 });
 
 app.listen(3000, () => {
   console.log('✅ Server running on port 3000');
 });
-
